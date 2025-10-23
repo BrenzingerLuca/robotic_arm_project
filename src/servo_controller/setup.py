5d1961552b1dd949686c4e650f71bbc86cee68ad
LucaB <luca.brenzinger@gmail.com>
2025-10-23 19:03:39 +0200

    Another try to controll the Servos this time using: python3-gpiozero

---
+++ src/servo_controller/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'servo_controller'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='lucab',
+    maintainer_email='luca.brenzinger@gmail.com',
+    description='ROS 2 package for controlling a servo motor with Raspberry Pi GPIO',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'servo_node = servo_controller.servo_node:main',
+        ],
+    },
+)
